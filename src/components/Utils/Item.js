23036bb3890ca44b662cc15b3ab0fe72e8c5ef1c
Michael Hughes <taurussho89@gmail.com>
2018-08-22 02:14:53 -0600

    Fixed Azerite Traits showing on Wowhead Tooltips, item name now colored by quality, and Azerite Level showing on Necklace

--- src/components/Utils/Item.js
+++ src/components/Utils/Item.js
@@ -22,26 +22,35 @@ export default class Item extends Component {
             });
         }
 
-        const borderColor = (quality) => {
+        const azeriteArray = (x, bonusArray) => {
+            wowhead += x;
+            bonusArray.forEach((bonus, index) => {
+                if (index > 0) {
+                    wowhead += `:${bonus.id}`
+                }
+            });
+        }
+
+        const qualityColor = (quality) => {
             switch (quality) {
 
                 case 1:
-                    return '0 0 0 1px #ffffff, 0 0 2px #ffffff'
+                    return '#ffffff'
                 
                 case 2:
-                    return '0 0 0 1px #02ff4e, 0 0 2px #02ff4e'
+                    return '#02ff4e'
 
                 case 3:
-                    return '0 0 0 1px #0281ff, 0 0 2px #0281ff'
+                    return '#0281ff'
 
                 case 4:
-                    return '0 0 0 1px #c600ff, 0 0 4px #c600ff'
+                    return '#c600ff'
                 
                 case 5:
-                    return '0 0 0 1px #ff8002, 0 0 12px #ff8002'
+                    return '#ff8002'
 
                 case 6:
-                    return '0 0 0 1px #e5cc80, 0 0 12px #e5cc80'
+                    return '#e5cc80'
         
                 default:
                     return null
@@ -49,25 +58,22 @@ export default class Item extends Component {
         }
 
         item.bonusLists && bonusArray(`&amp;bonus=${item.bonusLists[0]}`, item.bonusLists);
-        item.azeriteEmpoweredItem.azeritePowers && bonusArray(`&amp;azerite-powers=${selectedClass}`, item.azeriteEmpoweredItem.azeritePowers);
+        item.azeriteEmpoweredItem.azeritePowers && azeriteArray(`&amp;azerite-powers=${selectedClass}`, item.azeriteEmpoweredItem.azeritePowers);
         item.itemLevel && addWowheadData(`&amp;ilvl=${item.itemLevel}`);
         item.tooltipParams.enchant && addWowheadData(`&amp;ench=${item.tooltipParams.enchant}`);
         item.tooltipParams.gem0 && addWowheadData(`&amp;gems=${item.tooltipParams.gem0}`);
         item.tooltipParams.gem1 && addWowheadData(`:${item.tooltipParams.gem1}`);
         item.tooltipParams.gem2 && addWowheadData(`:${item.tooltipParams.gem2}`);
-        //let sock = 0; //Extra Socket
         item.tooltipParams.set && bonusArray(`&amp;pcs=${item.tooltipParams.set[0]}`, item.tooltipParams.set);
         //let upgd = 0; //Upgrade level, seemingly not used?
-
-        let borderGlow = borderColor(item.quality);
-
 
         return (
                 <a className="item-link" href={link} data-wowhead={wowhead} target="_blank">
                     <div className="item-container">
-                        <div className="item-name">{item.name}</div>
-                        <div className="item-icon" style={{backgroundImage: `url('${icon}')`, boxShadow: borderGlow}} />
+                        <div className="item-name" style={{color: qualityColor(item.quality)}}>{item.name}</div>
+                        <div className="item-level">Item Level {item.itemLevel} {item.name === 'Heart of Azeroth' && `(Azerite Level ${item.azeriteItem.azeriteLevel})`}</div>
                     </div>
+                    <div className="item-icon" style={{backgroundImage: `url('${icon}')`, boxShadow: `0 0 0 1px ${qualityColor(item.quality)}, 0 0 2px ${qualityColor(item.quality)}`}} />
                 </a>
         )
     }
