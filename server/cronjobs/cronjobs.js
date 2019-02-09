8af0d57d3c84c7689637bd5f3707267041e53b39
unknown <taurussho89@gmail.com>
2019-02-08 21:27:20 -0700

    Created hourly cronjobs for all Blizzard controller master lists & token

--- server/cronjobs/cronjobs.js
+++ server/cronjobs/cronjobs.js
@@ -1,10 +1,13 @@
+//Controllers
 const blizzardApi = require('../controllers/blizzard_api_controller');
 const raiderioApi = require('../controllers/raiderio_api_controller');
 const wowprogressApi = require('../controllers/wowprogress_api_controller');
 const blizztrackApi = require('../controllers/Blizztrack/blizztrack_api_controller');
 
+//Cron Library https://www.npmjs.com/package/cron
 const CronJob = require('cron').CronJob;
 
+//Every x minutes
 const minutes =  {
     every5: () => new CronJob('00 */5 * * * *', () => {
         blizzardApi.setServerStatus();
@@ -25,16 +28,27 @@ const minutes =  {
     }, null, false, 'America/Denver').start(),
 }
 
-// const hours = {
+//Every x hours
+const hours = {
+    every: () => new CronJob('* 0 */1 * * *', () => {
+        blizzardApi.setAchievements();
+        blizzardApi.setBlizzardToken();
+        blizzardApi.setClasses();
+        blizzardApi.setMounts();
+        blizzardApi.setPetTypes();
+        blizzardApi.setPets();
+        blizzardApi.setRaces();
+    }, null, false, 'America/Denver').start(),
+}
 
-// }
-
+//Export cron jobs so server starts them
 module.exports = {
 
     jobs: () => {
         minutes.every5();
         minutes.every10();
         minutes.every30();
+        hours.every();
     }
 
 }
