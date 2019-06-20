845e850334c40add049b14fda2afa5b92adcf1d7
Michael Hughes <TaurusSHO89@gmail.com>
2019-06-19 21:59:31 -0600

    Removed data variables in favor of retrieving them from db. Changed all get requests to use db.

--- server/controllers/raiderio_api_controller.js
+++ server/controllers/raiderio_api_controller.js
@@ -1,29 +1,22 @@
-const axios = require('axios');
-let raiderIOObj = {};
-let raiderIOAffixes = {};
-
 module.exports = {
-    setWowRankingsGuild: (req, res) => {
-        axios.get(`https://raider.io/api/v1/guilds/profile?region=us&realm=Thunderlord&name=complexity&fields=raid_progression%2Craid_rankings`).then(response => {
-            raiderIOObj = response.data;
-        }).catch(raiderIOError => {
-            console.log('Raider IO Get Error', raiderIOError);
-        });
-    },
 
     getWowRankingsGuild: (req, res) => {
-            res.send(raiderIOObj).status(200);
-    },
-
-    setWowMythicAffixes: (req, res) => {
-        axios.get('https://raider.io/api/v1/mythic-plus/affixes?region=us&locale=en').then(response => {
-            raiderIOAffixes = response.data;
-        }).catch(raiderIOAffixError => {
-            console.log('Raider IO Affix Get Error', raiderIOAffixError);
+        req.app.get('db').wowcache.findOne({id: 14}).then(response => {
+            res.status(200).send(response.body.data);
+        }).catch(err => {
+            console.log('DB RaiderIO Wow Guild Rank Error');
+            console.log(error);
+            res.status(500).send('DB RaiderIO Wow Guild Rank Error');
         });
     },
 
     getWowMythicAffixes: (req, res) => {
-            res.send(raiderIOAffixes).status(200);
+        req.app.get('db').wowcache.findOne({id: 15}).then(response => {
+            res.status(200).send(response.body.data);
+        }).catch(err => {
+            console.log('DB RaiderIO Wow Mythic Affixes Error');
+            console.log(error);
+            res.status(500).send('DB RaiderIO Wow Mythic Affixes Error');
+        });
     },
 }
