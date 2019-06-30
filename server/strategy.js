1a9a786cd5dd67fede36c0ea7b005b3bb96eb2a1
Michael Hughes <TaurusSHO89@gmail.com>
2019-06-30 15:06:41 -0600

    Added Dev Environment check from ENV.

--- server/strategy.js
+++ server/strategy.js
@@ -1,12 +1,12 @@
 //Bnet Strategy from (https://github.com/Blizzard/passport-bnet)
 const BnetStrategy = require('passport-bnet');
-const { BLIZZ_API_CLIENT_ID, BLIZZ_API_CLIENT_SECRET } = process.env;
+const { BLIZZ_API_CLIENT_ID, BLIZZ_API_CLIENT_SECRET, DEV } = process.env;
 
 module.exports = new BnetStrategy({
     clientID: BLIZZ_API_CLIENT_ID,
     clientSecret: BLIZZ_API_CLIENT_SECRET,
     scope: "wow.profile sc2.profile",
-    callbackURL: "https://localhost:3050/auth/bnet/callback"
+    callbackURL: DEV ? "https://localhost:3050/auth/bnet/callback" : "https://www.complexityguild.net/auth/bnet/callback"
 },
 function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
