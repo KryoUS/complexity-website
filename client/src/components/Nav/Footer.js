72de9114cdba5c8cad520e74ed133476febaa1ef
Michael Hughes <TaurusSHO89@gmail.com>
2019-09-25 20:49:53 -0600

    Fixed various formatting issues.

--- client/src/components/Nav/Footer.js
+++ client/src/components/Nav/Footer.js
@@ -1,4 +1,4 @@
-import React,  { Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { infoModal } from '../../ducks/reducer';
 import axios from 'axios';
@@ -16,22 +16,22 @@ class Footer extends Component {
 
     componentDidMount = () => {
         axios.get('/api/wow/token/price').then(res => {
-            this.setState({ tokenPrice: res.data.price.toString().slice(0,-4)})
+            this.setState({ tokenPrice: res.data.price.toString().slice(0, -4) })
         }).catch(error => {
             this.props.infoModal(true, 'Oops!', "We asked for the WoW Token Price but never heard back, stupid Goblins. Please try again in a moment.", 'OK');
         });
     }
 
-    render(){
+    render() {
 
-        return(
+        return (
             <div className="nav-footer">
                 <MythicPlusAffixes />
-                <div style={{fontSize: '10px', color: '#585858'}}>World of Warcraft and Blizzard Entertainment are trademarks or registered trademarks of Blizzard Entertainment, Inc. in the U.S. and/or other countries. All other trademarks are the property of their respective owners.</div>
-                <div style={{width: '330px', fontSize: '12px', color: 'white', textAlign: 'center'}}>
-                    {this.state.tokenPrice > 0 && 
-                        <a href="https://us.shop.battle.net/en-us/product/world-of-warcraft-token" data-wowhead="item=122284" target="_blank"  rel="noopener noreferrer">
-                            <img style={{width: '12px', height: '12px'}} alt='Current WoW Token Price' src="https://res.cloudinary.com/complexityguild/image/upload/v1538802480/site/iconarmory.png" />
+                <div style={{ fontSize: '10px', color: '#585858' }}>World of Warcraft and Blizzard Entertainment are trademarks or registered trademarks of Blizzard Entertainment, Inc. in the U.S. and/or other countries. All other trademarks are the property of their respective owners.</div>
+                <div style={{ width: '330px', fontSize: '12px', color: 'white', textAlign: 'center' }}>
+                    {this.state.tokenPrice > 0 &&
+                        <a href="https://us.shop.battle.net/en-us/product/world-of-warcraft-token" data-wowhead="item=122284" target="_blank" rel="noopener noreferrer">
+                            <img style={{ width: '12px', height: '12px' }} alt='Current WoW Token Price' src="https://res.cloudinary.com/complexityguild/image/upload/v1538802480/site/iconarmory.png" />
                             Token: {this.state.tokenPrice}g
                         </a>
                     }
@@ -41,7 +41,7 @@ class Footer extends Component {
     }
 }
 
-const mapStateToProps = ( state ) => {
+const mapStateToProps = (state) => {
     return {
         modalOpen: state.modalOpen,
         modalTitle: state.modalTitle,
@@ -50,4 +50,4 @@ const mapStateToProps = ( state ) => {
     }
 }
 
-export default connect( mapStateToProps, {infoModal} )( Footer );
+export default connect(mapStateToProps, { infoModal })(Footer);
