1cf8afe0bbd40166272cd7ab25474d26f1ed8c6e
Michael Hughes <TaurusSHO89@gmail.com>
2019-09-25 18:58:38 -0600

    Added Loader Component for easier loading changes.

---
+++ client/src/components/Utils/Loader.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Loader extends Component {
+
+    render() {
+        return (
+            <div className="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        )
+    }
+}
+
+export default Loader;
