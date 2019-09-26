1fe32dde11fc95ab816350997d56f438559c91b6
Michael Hughes <TaurusSHO89@gmail.com>
2019-09-26 01:01:58 -0600

    Added item quality to HEX function.

--- server/controllers/Database/tools/functions.js
+++ server/controllers/Database/tools/functions.js
@@ -12,5 +12,26 @@ module.exports = {
             }
             return value;
         };
-    }
+    },
+
+    qualityColor: (quality) => {
+        switch (quality) {
+            case 1:
+                return '#ffffff'
+            case 2:
+                return '#02ff4e'
+            case 3:
+                return '#0281ff'
+            case 4:
+                return '#c600ff'
+            case 5:
+                return '#ff8002'
+            case 6:
+                return '#e5cc80'
+            case 7:
+                return '#0cf'
+            default:
+                return null
+        }
+    },
 }
