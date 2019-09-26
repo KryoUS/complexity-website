6f8538b96e7364c962c3dfaac8068fa6623bcfee
Michael Hughes <TaurusSHO89@gmail.com>
2019-09-25 20:14:34 -0600

    Modified Loader to be able to scale based on props.

--- client/src/components/Utils/Loader.js
+++ client/src/components/Utils/Loader.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class Loader extends Component {
+    constructor() {
+        super();
+
+    }
 
     render() {
         return (
-            <div className="lds-roller">
+            <div className="lds-roller" style={{transform: `scale(${this.props.scale})`, margin: this.props.margin}} >
                 <div></div>
                 <div></div>
                 <div></div>
