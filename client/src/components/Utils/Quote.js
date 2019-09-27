ee7001123e0c61c8201248b5f705b62571778153
Michael Hughes <TaurusSHO89@gmail.com>
2019-09-26 19:05:59 -0600

    Added the ability for the Random Quote to fetch a new one when clicked.

--- client/src/components/Utils/Quote.js
+++ client/src/components/Utils/Quote.js
@@ -14,18 +14,23 @@ class Quote extends Component {
         }
     }
 
-    componentDidMount = () => {
+    getRandomQuote = () => {
+        this.setState({ showQuote: false });
         axios.get('/api/complexity/quotes').then(res => {
             this.setState({ quote: res.data.quote, saidBy: res.data.said_by, showQuote: true });
         }).catch(err => {
             console.log('Quote retrieve error.');
         });
-    }
+    };
+
+    componentDidMount = () => {
+        this.getRandomQuote();
+    };
 
     render() {
         return (
             this.state.showQuote ? 
-            <div className="fade1s" style={{ margin: '5px', fontSize: '1.25rem', textAlign: 'center', textShadow: '2px 2px 3px #000000' }}>"{this.state.quote}" - {this.state.saidBy}</div>
+            <div className="random-quote fade1s" style={{ margin: '5px', fontSize: '1.25rem', textAlign: 'center', textShadow: '2px 2px 3px #000000' }} onClick={this.getRandomQuote} >"{this.state.quote}" - {this.state.saidBy}</div>
             :
             <Loader scale={'0.25'} margin={'-12px'} />
         )
