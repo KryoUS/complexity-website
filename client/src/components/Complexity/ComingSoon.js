7104ed7405b9c078d67e52206e563b6e837f6f75
Michael Hughes <TaurusSHO89@gmail.com>
2019-09-28 01:58:40 -0600

    Updated to use new Quote component.

--- client/src/components/Complexity/ComingSoon.js
+++ client/src/components/Complexity/ComingSoon.js
@@ -1,25 +1,9 @@
 import React,  { Component } from 'react';
 import axios from 'axios';
+import Quote from '../Utils/Quote';
 import '../News/News.css';
 
 class ComingSoon extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            quote: '',
-            saidBy: '',
-            showQuote: false
-        }
-    }
-
-    componentDidMount = () => {
-        axios.get('/api/complexity/quotes').then(res => {
-            this.setState({quote: res.data.quote, saidBy: res.data.said_by, showQuote: true});
-        }).catch(err => {
-            console.log('Quote retrieve error.');
-        });
-    }
 
     render(){
         return(
@@ -30,7 +14,7 @@ class ComingSoon extends Component {
                         <div className='fade2s' style={{height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
                             <img className="logo" src="https://res.cloudinary.com/complexityguild/image/upload/v1535585345/wow/logos/logov3.png" alt="Complexity Logo"/>
                             <div className='modal-title' style={{fontSize: '4rem', textAlign: 'center'}}>COMING SOON</div>
-                            <div className='pulse' style={{fontSize: '1.5rem', textShadow: '2px 2px 3px #000000', textAlign: 'center'}}>{this.state.showQuote && `${this.state.quote} - ${this.state.saidBy}`}</div>
+                            <Quote />
                         </div>
                     </div>
                 </div>
