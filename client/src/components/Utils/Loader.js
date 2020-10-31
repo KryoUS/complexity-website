73d4dcb5c47d54b1cd8ddc9bb4f1287c5f6855d9
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-31 01:37:34 -0600

    Fixed missing css property for component.

--- client/src/components/Utils/Loader.js
+++ client/src/components/Utils/Loader.js
@@ -4,7 +4,7 @@ class Loader extends Component {
 
     render() {
         return (
-            <div className="lds-roller" style={{transform: `scale(${this.props.scale})`, margin: this.props.margin}} >
+            <div className="lds-roller" style={{transform: `scale(${this.props.scale})`, margin: this.props.margin, overflow: 'hidden'}} >
                 <div></div>
                 <div></div>
                 <div></div>
