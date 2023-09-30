122a03c27d9ae9708942b573696c782a286c7f06
KryoUS <taurussho89@gmail.com>
2023-09-30 16:11:27 -0600

    Added new Bnet Interceptor logging to DB.

---
+++ server/controllers/Database/logging/bnetLogging.js
@@ -0,0 +1,28 @@
+const { app } = require('../../../app');
+
+module.exports = {
+    bnetLogging: async (status, statusText, method, baseurl, url, message) => {
+        let db = await app.get('db');
+
+        db.bnetlog.insert({
+            status: status,
+            statustext: statusText,
+            method: method,
+            baseurl: baseurl,
+            url: url,
+            message: message
+        }).then(result => {
+            //Do nothing with results
+        }).catch(error => {
+            console.log(`${new Date()} ===Massive.js DB Bnet Log Insert Error===`, error);
+        })
+    },
+
+    bnetLoggingCleanup: async () => {
+        let db = await app.get('db');
+
+        db.query("DELETE FROM bnetlog WHERE created_at < NOW() - INTERVAL '15 days'").catch(dbError => {
+            console.log(`${new Date()} ===Massive.js DB Bnet Log Cleanup Error===`, dbError);
+        });
+    }
+}
