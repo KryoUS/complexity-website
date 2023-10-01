0207201cbc19916a8a778d7d1664aabdc93b0e75
KryoUS <taurussho89@gmail.com>
2023-10-01 16:57:36 -0600

    Fixed post ordering.

--- client/src/components/News/BluePosts.js
+++ client/src/components/News/BluePosts.js
@@ -14,27 +14,52 @@ export default class BluePosts extends Component {
 
     componentDidMount = () => {
         axios.get('/api/wow/blueposts').then(res => {
-            this.setState({bluePosts: res.data});
+            this.setState({ bluePosts: res.data });
         }).catch(error => {
 
         });
     }
 
+    post = (obj) => {
+        return <Link id="blizzard" href={`https://us.forums.blizzard.com/en/wow${obj.url}`} underline="hover" style={{ textOverflow: "ellipsis" }}>
+            <Typography align='left' variant='body1'>{moment(obj.created_at).fromNow(true)}: {obj.title}</Typography>
+        </Link>
+    }
+
     render() {
         return (
-            <Grid container>
-            {this.state.bluePosts.length >= 1 ?
-                this.state.bluePosts.map(obj => {
-                    return <Grid key={obj.id} item xs={6}>
-                        <Link id="blizzard" href={`https://us.forums.blizzard.com/en/wow${obj.url}`} underline="hover" style={{textOverflow: "ellipsis"}}>
-                            <Typography align='left' variant='body1'>{moment(obj.created_at).fromNow(true)}: {obj.title}</Typography>
-                        </Link>
+            <div>
+                {this.state.bluePosts.length >= 1 ?
+                    <Grid container>
+                        <Grid item xs={6}>
+                            <div>{this.post(this.state.bluePosts[0])}</div>
+                            <div>{this.post(this.state.bluePosts[1])}</div>
+                            <div>{this.post(this.state.bluePosts[2])}</div>
+                            <div>{this.post(this.state.bluePosts[3])}</div>
+                            <div>{this.post(this.state.bluePosts[4])}</div>
+                            <div>{this.post(this.state.bluePosts[5])}</div>
+                            <div>{this.post(this.state.bluePosts[6])}</div>
+                            <div>{this.post(this.state.bluePosts[7])}</div>
+                            <div>{this.post(this.state.bluePosts[8])}</div>
+                            <div>{this.post(this.state.bluePosts[9])}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div>{this.post(this.state.bluePosts[10])}</div>
+                            <div>{this.post(this.state.bluePosts[11])}</div>
+                            <div>{this.post(this.state.bluePosts[12])}</div>
+                            <div>{this.post(this.state.bluePosts[13])}</div>
+                            <div>{this.post(this.state.bluePosts[14])}</div>
+                            <div>{this.post(this.state.bluePosts[15])}</div>
+                            <div>{this.post(this.state.bluePosts[16])}</div>
+                            <div>{this.post(this.state.bluePosts[17])}</div>
+                            <div>{this.post(this.state.bluePosts[18])}</div>
+                            <div>{this.post(this.state.bluePosts[19])}</div>
+                        </Grid>
                     </Grid>
-                })
-                :
-                <CircularProgress />
-            }
-            </Grid>
+                    :
+                    <CircularProgress />
+                }
+            </div>
         )
     }
 }
