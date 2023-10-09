fa05cdbdb824e996b726f208386a374b53e917ee
KryoUS <taurussho89@gmail.com>
2023-10-09 04:54:55 -0600

    Remade Twitter Embed. Added Hook for Twitter Embed.

---
+++ client/src/components/News/twitter/TwitterEmbed.js
@@ -0,0 +1,19 @@
+import useScript from '../../hooks/useScript';
+
+function TwitterEmbed() {
+    useScript('https://platform.twitter.com/widgets.js');
+    return (
+        // eslint-disable-next-line
+        <a 
+            class="twitter-timeline"
+            data-theme="dark"
+            href="https://twitter.com/Warcraft?ref_src=twsrc%5Etfw"
+            // data-tweet-limit={14}
+            data-chrome="noheader nofooter noborders transparent"
+            charSet='utf-8'
+            alt="twitter embed"
+        />
+    )
+}
+
+export default TwitterEmbed;
